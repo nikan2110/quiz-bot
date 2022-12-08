a61483142cb9c53f585f79ed01ab023961100f0d
Nikita <nikan2110@gmail.com>
2022-12-08 16:22:41 +0200

    add heroku settings

--- main.py
+++ main.py
@@ -3,11 +3,23 @@ import inline_keyboard
 import tools
 import constants
 import API
+import os
 from pymongo import MongoClient
 from aiogram import Bot, Dispatcher, executor, types, utils
+from aiogram.utils.executor import start_webhook
 
 # Credentials
 API_TOKEN = tools.get_from_env("TELEGRAM_API_TOKEN")
+HEROKU_APP_NAME=tools.get_from_env("HEROKU_APP_NAME")
+
+# webhook settings
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{API_TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+# webserver settings
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.getenv('PORT', default=8000)
 
 # Question for user
 QUESTIONS = {}
@@ -159,6 +171,21 @@ async def call_a_friend(callback_query: types.CallbackQuery):
     await bot.send_photo(chat_id=callback_query.from_user.id, photo=constants.IMAGE_URL)
     await bot.send_message(chat_id=callback_query.from_user.id, text=f"Kanye: {quote}")
 
+async def on_startup(dispatcher):
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+
+
+async def on_shutdown(dispatcher):
+    await bot.delete_webhook()
+
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
